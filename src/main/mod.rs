0f50cca2741c8aec150a7ce14547007bb7d7b8f8
SamyAlderson <samysam35000@gmail.com>
2026-07-21 15:21:58 +0200

    cleaner interface now

---
+++ src/main/mod.rs
@@ -0,0 +1,15 @@
+// Module declaration for main.rs
+pub mod main;
+
+// Module declaration for compressor.rs
+pub mod compressor {
+    pub mod compressor;
+    pub mod encoder;
+    pub mod decoder;
+}
+
+// Module declaration for utils.rs
+pub mod utils {
+    pub mod bit_utils;
+    pub mod byte_utils;
+}
